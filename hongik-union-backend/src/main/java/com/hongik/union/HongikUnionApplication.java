e7f8adc201af162b0b3ba8cfc5416b852e2c0622
minimin0416 <missy707200@gmail.com>
2026-06-30 18:06:10 +0900

    Add Spring Boot backend (Java 21)

---
+++ hongik-union-backend/src/main/java/com/hongik/union/HongikUnionApplication.java
@@ -0,0 +1,11 @@
+package com.hongik.union;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongikUnionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HongikUnionApplication.class, args);
+    }
+}
